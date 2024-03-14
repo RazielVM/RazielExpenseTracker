12d67bd1bab126fe716e91e7770b84ebf00edbd6
RVazquez <raziel.vm12@gmail.com>
2024-03-13 19:00:33 -0600

    Add project files.

---
+++ Expense Tracker/Migrations/20240313230718_NTabla1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    public partial class NTabla1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Categories",
+                type: "nvarchar(7)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(6)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Categories",
+                type: "nvarchar(6)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(7)");
+        }
+    }
+}
